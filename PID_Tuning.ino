4b0c455d5fa371b468f7b81b6b77a6c224ad7abf
saifalikabi <33541954+saifalikabi@users.noreply.github.com>
2019-04-11 18:31:56 +0300

    The Last Update for CU3

--- PID_Tuning.ino
+++ PID_Tuning.ino
@@ -1,66 +1,65 @@
 #include <PID_v1.h>
-
-
 #include <LiquidCrystal.h>
-
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-
-
-
-
 const int photores = A0;   // LDR input pin
-const int pot = A1;    // Potentiometer input pin
-const int potP = A3; 
-const int potI = A4;
-const int potD = A5;
+const int pot = A1;        // Potentiometer input pin
 const int led = 9;         // LED output pin
-double lightLevel;         // Indirectly store the light level
+double lightLevel;       // Indirectly store the light level
+const double KpPin = A3 ;
+const double KiPin = A4 ;
+const double KdPin = A5 ;
+
 // Tuning parameters
-float Kp = 0;         // Proportional gain
-float Ki = 0;             // Integral gain
+float Kp = 0;              // Proportional gain
+float Ki = 10;             // Integral gain
 float Kd = 0;              // Differential gain
+
 // Record the set point as well as the controller input and output
 double Setpoint, Input, Output;
+
 // Create a controller that is linked to the specified Input, Ouput and Setpoint
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 const int sampleRate = 1;        // Time interval of the PID control
 const long serialPing = 500;     // How often data is recieved from the Arduino
 unsigned long now = 0;           // Store the time that has elapsed
 unsigned long lastMessage = 0;   // The last time that data was recieved
-int SetpointI, InputI, OutputI;
-
+ int SetpointI, InputI, OutputI;
 
 void setup()
  {
+  Kp = analogRead(KpPin) / 100.0; 
+  Ki = analogRead(KiPin) / 100.0; 
+  Kd = analogRead(KdPin) / 100.0; 
  lightLevel = analogRead(photores);                 // Read the set point
+ 
  // Arduino has an analogueRead() resolution of 0-1023 and an analogueWrite() resolution of 0-255
  Input = map(lightLevel, 0, 1023, 0, 255);        // Scale the input
- Setpoint = map(analogRead(pot), 0, 1023, 0, 255);     // Scale the set point
-Kp = map(analogRead(potP), 0, 1023, 0, 100); 
-Ki = map(analogRead(potI), 0, 1023, 0, 100); 
-Kd = map(analogRead(potD), 0, 1023, 0, 100); 
+ Setpoint = map(analogRead(pot), 0, 1023, 0, 255);  // Scale the set point
+ myPID.SetTunings(Kp, Ki, Kd);
  Serial.begin(9600);                                // Initialise serial communications at 9600 bps
  myPID.SetMode(AUTOMATIC);                          // Turn on the PID control
  myPID.SetSampleTime(sampleRate);                   // Assign the sample rate of the control
  Serial.println("Begin");                           // Let the user know that the set up s complete
- lastMessage = millis();   
- myPID.SetTunings(Kp, Ki, Kd);
-  lcd.begin(16, 4);
+ lastMessage = millis();                            // Serial data will be recieved relative to this first point
+ 
+ 
+ lcd.begin(16, 4);
   lcd.setCursor(4, 0);
  lcd.print("PID  HMI");
-// Serial data will be recieved relative to this first point
+ 
+ 
+ 
  }
   
 void loop()
  {
- Setpoint = map(analogRead(pot), 0, 1023, 0, 255);    // Continue to read and scale the set point
-Kp = map(analogRead(potP), 0, 1023, 0, 100);
-Ki = map(analogRead(potI), 0, 1023, 0, 100); 
-Kd = map(analogRead(potD), 0, 1023, 0, 100); 
+  Kp = analogRead(KpPin) / 100.0; 
+  Ki = analogRead(KiPin) / 100.0; 
+  Kd = analogRead(KdPin) / 100.0; 
+  myPID.SetTunings(Kp, Ki, Kd);
+ Setpoint = map(analogRead(pot), 0, 1023, 0, 255);  // Continue to read and scale the set point
  lightLevel = analogRead(photores);                 // Read the light level
  Input = map(lightLevel, 0, 1023, 0, 255);          // Scale the input to the PID
  myPID.Compute();                                   // Calculates the PID output at a specified sample time
@@ -106,8 +105,6 @@ lcd.setCursor(9, 3);
  lcd.print("D");
   lcd.setCursor(11, 3);
  lcd.print(Kd);
- 
- 
  Serial.print("\n");
  // The tuning parameters can be retrieved by the Arduino from the serial monitor: 0,0.5,0 set Ki to 0.5.
  // Commas are ignored by the Serial.parseFloat() command
@@ -140,18 +137,11 @@ lcd.setCursor(9, 3);
  Serial.print(Ki);
  Serial.print(",");
  Serial.print(Kd);
- myPID.SetTunings(Kp, Ki, Kd);  // Set the tuning of the PID loop
+// myPID.SetTunings(Kp, Ki, Kd);  // Set the tuning of the PID loop
  }
  lastMessage = now;               // Reference the next serial communication to this point
  }
-
-
-
  SetpointI = Setpoint;
  InputI =  Input ;
  OutputI =  Output ;
-
-
-
- 
  }
